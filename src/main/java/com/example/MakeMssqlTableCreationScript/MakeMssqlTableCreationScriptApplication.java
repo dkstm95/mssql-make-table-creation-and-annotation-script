d6e441596281bb8e4b7af30dd4b3e2895c988426
seungilahn <dkstm95@naver.com>
2021-08-30 11:09:41 +0900

    First Commit.

---
+++ src/main/java/com/example/MakeMssqlTableCreationScript/MakeMssqlTableCreationScriptApplication.java
@@ -0,0 +1,13 @@
+package com.example.MakeMssqlTableCreationScript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeMssqlTableCreationScriptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeMssqlTableCreationScriptApplication.class, args);
+	}
+
+}
